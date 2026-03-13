9392419a57cb521d784e5eb3aa21486b085e73c0
ehto5 <118539348+ehto5@users.noreply.github.com>
2026-03-13 15:53:47 -0500

    Add files via upload

---
+++ get_DA.ps1
@@ -0,0 +1,13 @@
+﻿$admins = get-adgroupmember 'domain admins' | get-aduser -properties * | select samaccountname,name,enabled,mail,whencreated
+ 
+$denygroup = Read-Host "Enter the name of the Deny Interactive Logon group, or leave blank to skip"
+ 
+#shows which DA accounts are also denied interactive login
+if ($denygroup) {
+    foreach ($admin in $admins) {
+        Write-host $admin.name
+        Get-ADPrincipalGroupMembership $admin.samaccountname | Where-object name -like $denygroup
+        }
+    }
+ 
+$admins | export-csv C:\temp\domainadmins.csv
