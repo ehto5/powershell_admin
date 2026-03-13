b4671a8f8ee1274887bc0c826617d2f6c48a5fa0
ehto5 <118539348+ehto5@users.noreply.github.com>
2026-03-13 15:54:22 -0500

    Add files via upload

---
+++ old_ad_comps.ps1
@@ -0,0 +1,12 @@
+﻿#adjust days inactive as needed in days
+$DaysInactive = 365
+$date = (Get-Date).AddDays(-($DaysInactive))
+#$computers = Get-ADComputer -Filter {LastLogonTimeStamp -lt $date} -ResultPageSize 2000 -ResultSetSize $null -Properties Name, OperatingSystem, SamAccountName, DistinguishedName, Created, Modified, whenChanged, whenCreated, lastlogondate, Enabled
+$computers = Get-ADComputer -Filter {LastLogonTimeStamp -lt $date} -ResultPageSize 2000 -ResultSetSize $null
+ 
+#export list to csv
+#$computers | Export-CSV "C:\temp\oldcomps.csv" -NoTypeInformation
+ 
+$TargetOU = Read-Host "Enter the full Distinguished Name of the target OU (e.g. OU=Disabled Computers,DC=domain,DC=com)"
+$computers | Move-ADObject -TargetPath $TargetOU
+ 
