68d901c82980b80a270adf593997ca91d8db3b0b
ehto5 <118539348+ehto5@users.noreply.github.com>
2026-03-13 15:53:56 -0500

    Add files via upload

---
+++ get_sneakypriv_accounts.ps1
@@ -0,0 +1,7 @@
+﻿$groups = Get-ADOrganizationalUnit -Filter * | %{(Get-ACL "AD:$($_.distinguishedname)").access} | Where-Object -Property ActiveDirectoryRights -contains "GenericAll" | Select-Object IdentityReference
+foreach ($group in $groups) {
+    $user = Get-ADGroupMember ([string]$group.IdentityReference).split('\')[1] | Select -Property name, samaccountname
+    $users +=$user
+    }
+       $users | Select -Property name, samaccountname -Unique | Export-Csv C:\temp\high_priv_users.csv
+ 
